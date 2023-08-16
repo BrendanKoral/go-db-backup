e29fa483c3b503602a4c3cd0cb3163a220afb59b
Brendan Koral <brendan@bkoral.io>
2023-08-16 15:09:36 -0500

    Remove unused struct

--- fs/fs.go
+++ fs/fs.go
@@ -2,16 +2,8 @@ package fs
 
 import (
 	"errors"
-	"fmt"
 	"os"
 )
-
-type Config struct {
-	DumpDir string
-	Test    string
-}
-
-var timeFormat = "20060102T150405"
 
 // CreateBackupDir creates the backup directory if it doesn't exist
 func CreateBackupDir(path string) error {
@@ -24,18 +16,6 @@ func CreateBackupDir(path string) error {
 	// This is ok, dir already exists
 	if _, err := os.Stat(path); errors.Is(err, os.ErrExist) {
 	}
-
-	return err
-}
-
-func DeleteInvalidBackupFile(path string, fileName string) error {
-	file := fmt.Sprintf("%s/%s", path, fileName)
-
-	if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
-		return err
-	}
-
-	err := os.Remove(file)
 
 	return err
 }
